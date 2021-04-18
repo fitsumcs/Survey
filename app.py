b8cd4723253fa424d52c1d2cbc5e452210dbcd04
Fitsum A <fitsum@gmail.com>
2021-04-18 11:11:54 +0300

    Configuring the page title and header text

---
+++ app.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+import streamlit as st 
+import plotly.express as px 
+from PIL import Image 
+
+# Set page title 
+st.set_page_config(page_title="Survey Data")
+st.header('Survey Result of 2022')
